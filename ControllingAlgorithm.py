f229fada6bac1bb360db1b6e68d86872d1a3864e
mathis van der steen <mathisvdersteen@hotmail.com>
2025-09-25 18:37:16 +0200

    added order of moves list for a path

--- ControllingAlgorithm.py
+++ ControllingAlgorithm.py
@@ -7,7 +7,15 @@ import OpenWebsiteAndStartGame as OWASG
 #Constant Variables
 FromStart = ["From Start", "from start", "*From Start*", "*from start*", "FromStart"]
 #move set after reaching specific point 1 = up, 2 = right, 3 = down, 4 = left
-FoundationalMoveSet = []
+#follow the paths one at a time 2A then 2B
+#start in the right in the bottom
+LeftRight = [1, 4, 1, 2, 1, 4, 1, 2, 1, 4, 1, 2, 1, 4, 1, 2, 1 4, 1, 2, 1, 4, 1, 2]
+#19keer 4
+UpDown = [1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4]
+FoundationalMoveSet1 = LeftRight + UpDown
+FoundationalMoveSet2A = [3, 4]
+FoundationalMoveSet2B = [3]
+FoundationalMoveSet3 = [2]
 
 #functions
 def Move(InputValue):
