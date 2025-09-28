bda0cabd395dec09ac5d9870814f9a03ff82b5ac
mathis van der steen <mathisvdersteen@hotmail.com>
2025-09-28 17:57:04 +0200

    complete controlling algorithm

--- ControllingAlgorithm.py
+++ ControllingAlgorithm.py
@@ -6,39 +6,72 @@ import OpenWebsiteAndStartGame as OWASG
 #variables
 #Constant Variables
 FromStart = ["From Start", "from start", "*From Start*", "*from start*", "FromStart"]
-#move set after reaching specific point 1 = up, 2 = right, 3 = down, 4 = left
+
+#sub move lists
+HorizantalSubList = [[4, 1], [3, 1], [4, 1], [1, 1]]
+VerticalSubList = [[1, 1], [4, 19], [1, 1], [2, 19]]
+
+#move set after reaching specific point 0=nothing/stay on course 1 = up, 2 = right, 3 = down, 4 = left
+StartMoveSet = []
+
 #follow the paths one at a time 2A then 2B
 #start in the right in the bottom
-LeftRight = [1, 4, 1, 2, 1, 4, 1, 2, 1, 4, 1, 2, 1, 4, 1, 2, 1 4, 1, 2, 1, 4, 1, 2]
+#first is direcion second is how many squires it moves
+LeftRight = [VerticalSubList*6]
 #19keer 4
-UpDown = [1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4, 3, 4, 1, 4]
+UpDown = [[1, 2], HorizantalSubList*9, [4, 1]]
 FoundationalMoveSet1 = LeftRight + UpDown
-FoundationalMoveSet2A = [3, 4]
-FoundationalMoveSet2B = [3]
-FoundationalMoveSet3 = [2]
+FoundationalMoveSet2A = [[3, 1], [4, 1], [3, 1]]
+FoundationalMoveSet2B = [[0, 1], [3, 2]]
+FoundationalMoveSet3 = [[0, 12], [2, 20]]
 
 #functions
 def Move(InputValue):
     if InputValue == 1:
         C.MoveUp()
     elif InputValue == 2:
-        MoveRight()
+        C.MoveRight()
     elif InputValue == 3:
-        MoveDown()
+        C.MoveDown()
     elif InputValue == 4:
-        MoveLeft()
+        C.MoveLeft()
     else:
         print("error: wrong input for the move set")
         pass
     pass
 
+def ExecuteMoveSet(Inputs):
+    Move(Inputs[0])
+    # wacht voor zoveel blokjes bewogen Inputs[1]
+    pass
+
+def ExecuteEachMove(InputsList):
+    for each in InputsList:
+        ExecuteMoveSet(each)
+        pass
+
 #Execution
 def ChooseWhatVersionToExecute():
+    global StartMoveSet
     ChooseInput = input("what version do you execute: ")
     if ChooseInput in FromStart:
+        StartMoveSet = [[0, 11], [2, 10]]
         pass
     pass
 
 while True:
     ChooseWhatVersionToExecute()
+    ExecuteEachMove(StartMoveSet)
+    MoveSet1 = True
+    while True:
+        ExecuteEachMove(FoundationalMoveSet1)
+        if MoveSet1:
+            ExecuteEachMove(FoundationalMoveSet2A)
+            MoveSet1 = False
+        else:
+            ExecuteEachMove(FoundationalMoveSet2B)
+            MoveSet1 = True
+            pass
+        ExecuteEachMove(FoundationalMoveSet3)
+        pass
     pass
