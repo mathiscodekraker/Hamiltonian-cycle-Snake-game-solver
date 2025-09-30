869c31a7eb1e529893c6ec5dc4dd71b13ebb9804
mathis van der steen <mathisvdersteen@hotmail.com>
2025-09-30 14:00:19 +0200

    image recognition base function

--- ImageRecognition.py
+++ ImageRecognition.py
@@ -1,5 +1,21 @@
+#library
+import OpenWebsiteAndStartGame as OWASG
+from OpenWebsiteAndStartGame import GameScreenSize
+from PIL import ImageGrab
+
 #screenshot devide image into grid 
 #- from PIL import Image https://codingfleet.com/transformation-details/splitting-an-image-into-a-grid-using-python/
 
+def SnakeMoved(img):
+    return
+    
+#function
 def MoveSetCompletion(SpacesHaveToMoveAmount, Direction):
+    for each in range(SpacesHaveToMoveAmount):
+        SnakeNotMoved = True
+        while SnakeNotMoved:
+            img = ImageGrab.grab(bbox=GameScreenSize)
+            SnakeNotMoved = not SnakeMoved(img)
+            pass
+        pass
     pass
---
+++ OpenWebsiteAndStartGame.py
@@ -0,0 +1,10 @@
+#library
+
+#variables
+#constant variables
+#590-1330, 289-819 (x=1919, y=1079)
+#590-(1919-1330), 289-(1079-819)
+#590-589, 289-260
+GameScreenSize = [590, 289, 589, 260]
+
+#functions
